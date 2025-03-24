234783df246c88386e595cb7a881749ede7aa083
Eric <eric.so.dev@gmail.com>
2025-03-24 01:18:43 -0400

    feat: implement evaluate infix expressions

--- rust/src/evaluator/evaluator.rs
+++ rust/src/evaluator/evaluator.rs
@@ -1,19 +1,41 @@
-use crate::parser::ast::{Boolean, Expression, Integer, Statement};
+use crate::{
+    lexer::tokenizer::TokenType,
+    parser::ast::{Boolean, Expression, InfixExpression, Integer, Statement},
+};
 
 use super::object::Object;
 
 impl Expression {
-    fn evaluate(self) -> Object {
+    fn evaluate(&self) -> Object {
         match self {
-            Expression::Integer(Integer(_, value)) => Object::Integer(value),
-            Expression::Boolean(Boolean(_, value)) => Object::Boolean(value),
+            Expression::Integer(Integer(_, value)) => Object::Integer(value.to_owned()),
+            Expression::Boolean(Boolean(_, value)) => Object::Boolean(value.to_owned()),
+            Expression::InfixExpression(expr) => {
+                let InfixExpression {
+                    left_expression: left_expr,
+                    right_expression: right_expr,
+                    operator,
+                } = expr.as_ref();
+                if let (Object::Integer(left_val), Object::Integer(right_val)) =
+                    (left_expr.evaluate(), right_expr.evaluate())
+                {
+                    return match operator.token_type {
+                        TokenType::Plus => Object::Integer(left_val + right_val),
+                        TokenType::Minus => Object::Integer(left_val - right_val),
+                        TokenType::Asterisk => Object::Integer(left_val * right_val),
+                        TokenType::Slash => Object::Integer(left_val / right_val),
+                        _ => Object::Null,
+                    };
+                }
+                Object::Null
+            }
             _ => todo!(),
         }
     }
 }
 
 impl Statement {
-    fn evaluate(self) -> Object {
+    fn evaluate(&self) -> Object {
         match self {
             Statement::ExpresssionStatement(expr) => expr.evaluate(),
             _ => todo!(),
