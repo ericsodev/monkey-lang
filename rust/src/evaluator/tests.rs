64c0a550b40e492cf3a21f3f3b0cb820608d7552
Eric <eric.so.dev@gmail.com>
2025-03-24 00:58:43 -0400

    tests: add prefix and infix expression tests

--- rust/src/evaluator/tests.rs
+++ rust/src/evaluator/tests.rs
@@ -22,4 +22,18 @@ mod tests {
         assert_eq!(eval_input("true"), Object::Boolean(true));
         assert_eq!(eval_input("false"), Object::Boolean(false));
     }
+
+    #[test]
+    fn test_infix_expression() {
+        assert_eq!(eval_input("3 + 1"), Object::Integer(4));
+        assert_eq!(eval_input("4 * 8"), Object::Integer(32));
+        assert_eq!(eval_input("4 * 8 + 10"), Object::Integer(42));
+        assert_eq!(eval_input("4 * (8 - 10) - 2"), Object::Integer(-42));
+    }
+
+    #[test]
+    fn test_prefix_expression() {
+        assert_eq!(eval_input("-3"), Object::Integer(-3));
+        assert_eq!(eval_input("-0"), Object::Integer(0));
+    }
 }
