4d646dc346ff6c6e24a594e8063f5d50f37f4ad8
Eric <eric.so.dev@gmail.com>
2025-03-24 21:59:41 -0400

    feat: implement prefix eval

--- rust/src/evaluator/evaluator.rs
+++ rust/src/evaluator/evaluator.rs
@@ -1,15 +1,39 @@
 use crate::{
     lexer::tokenizer::TokenType,
-    parser::ast::{Boolean, Expression, InfixExpression, Integer, Statement},
+    parser::ast::{Boolean, Expression, InfixExpression, Integer, PrefixExpression, Statement},
 };
 
 use super::object::Object;
+
+fn evaluate_bang_prefix_expression(value: Object) -> Object {
+    match value {
+        Object::Boolean(val) => Object::Boolean(!val),
+        _ => Object::Null,
+    }
+}
+
+fn evaluate_minus_prefix_expression(value: Object) -> Object {
+    match value {
+        Object::Integer(val) => Object::Integer(-val),
+        _ => Object::Null,
+    }
+}
 
 impl Expression {
     fn evaluate(&self) -> Object {
         match self {
             Expression::Integer(Integer(_, value)) => Object::Integer(value.to_owned()),
             Expression::Boolean(Boolean(_, value)) => Object::Boolean(value.to_owned()),
+            Expression::PrefixExpression(expr) => {
+                let PrefixExpression { operator, expression } = expr.as_ref();
+                let val = expression.evaluate();
+
+                match operator.token_type {
+                    TokenType::Bang => evaluate_bang_prefix_expression(val),
+                    TokenType::Minus => evaluate_minus_prefix_expression(val),
+                    _ => Object::Null,
+                }
+            }
             Expression::InfixExpression(expr) => {
                 let InfixExpression {
                     left_expression: left_expr,
